045c6e244d2b9e40150c6debb8ba32a94bb7db29
Kothai Purushothaman <kothaipurushothaman@email.com>
2025-06-17 23:49:42 -0400

    Fix for Render port 502 error

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 @app.route('/')
 def home():
     return "Welcome to AI Dungeon Master!"
@@ -44,4 +46,9 @@ def next_scene():
     })
 
 if __name__ == '__main__':
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
